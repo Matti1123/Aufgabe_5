245190da4cb01d84f6d489e3589f49dfc9a26358
matti0101 <fm9023@mci4me.at>
2024-04-28 19:42:03 +0200

    Aufgabe6.2

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
-import my_functions
+import my_functions as mf
 import json
+import my_classes as mc
+import numpy as np
 
-experiment_name = input("Geben sie Experimentname:")
-supervisor = input("Geben supervisor ein:")
-date = input("Geben date ein:")
-subject = input("Geben subject ein:")
 
 
 
 if __name__ == "__main__":
-    experiment = my_functions.build_experiment(experiment_name,supervisor,date,subject)
+    # Experimentdaten
+    experiment_input = input("Geben sie Experimentname:")
+    date = input("Geben date ein:")
+
+
+    #Personendaten subject
+    first_name = input("Geben sie first_name:")
+    last_name = input("Geben sie last_name ein:")
+    sex = input("Geben sex ein:")
+    age = int(input("Geben age ein:"))
+    birthdate = input("Geben birthdate ein:")
+
+    #Personendaten supervisor
+    supervisor_fist_name = input("Geben sie Supervisor first_name:")
+    supervisor_last_name = input("Geben sie Supervisor last_name:")
+    supervisor_id = input("Geben sie supervisor_id ein:")
+
+
+    subject = mc.Subject(first_name, last_name, sex, age,birthdate)
+    mc.Subject.save(subject)
+    print(subject)
+    
+    supervisor = mc.Supervisor(supervisor_fist_name, supervisor_last_name, supervisor_id)
+    mc.Supervisor.save(supervisor)
+    print(supervisor)
+
+    experiment = mc.Experiment(experiment_name = experiment_input,date = date ,subject = subject ,supervisor=supervisor)
     print(experiment)
+    mc.Experiment.save(experiment)
 
-
-with open("sample.json", "a") as outfile: 
-    json.dump(experiment, outfile)
-
-
+#with open("main.json", "a") as outfile: 
+    #json.dump(experiment, outfile)
---
+++ my_classes.py
@@ -0,0 +1,68 @@
+import json
+
+class Person:
+    def __init__(self, first_name:str,last_name:str):
+        self.first_name = first_name
+        self.last_name = last_name
+    
+    def save(self):
+        with open("person.json", "a") as outfile: 
+            json.dump(self.__dict__, outfile)
+
+
+class Subject(Person):
+    def __init__(self,first_name:str,last_name:str,sex:str,age:int,birthdate:int):
+        super().__init__(first_name,last_name)
+        self.sex = sex
+        self.age = age
+        self.__birthdate = birthdate
+        self.max_hr = self.estimate_max_hr(self.sex,self.age)
+
+    #def calculate_age(self):
+
+        
+
+    def estimate_max_hr(self,sex,age):
+        if sex == "male":
+            max_hr_bpm =  223 - 0.9 * age
+        elif sex == "female":
+            max_hr_bpm = 226 - 1.0 *  age
+        else:
+            # der input() öffnet ein Eingabefenster für den Nutzer und speichert die Eingabe
+            max_hr_bpm  = input("Enter maximum heart rate:")
+        return max_hr_bpm
+
+
+
+class Supervisor(Person):
+    def __init__(self,first_name:str,last_name:str,supervisor_id:int):
+        super().__init__(first_name = first_name,last_name = last_name)
+        self.id = supervisor_id
+        
+
+
+class Experiment:
+    def __init__(self,experiment_name:str,date:str,subject:Subject, supervisor:Supervisor):
+        self.experiment_name = experiment_name
+        self.date = date 
+        self.supervisor_data = {
+            "first_name" : supervisor.first_name,
+            "last_name" : supervisor.last_name,
+            "supervisor_id" : supervisor.id,
+        }
+        self.subject_data = {
+            "first_name" : subject.first_name,
+            "last_name" : subject.last_name,
+        }
+
+    def save(self):
+        with open("experiment.json", "a") as outfile: 
+            json.dump(self.__dict__, outfile)
+
+            
+"""Person1 = Subject("Max","Must","male",25,1966)
+print(Person1.estimate_max_hr)
+Person1.save()
+
+s1 = Supervisor("Domi","Dulli",3)
+print(s1.__dict__)"""
